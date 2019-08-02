0db8fcad98175327ebd88b725c01b648fdd7c54c
Jack Fletcher <jdfletch97@gmail.com>
2019-08-02 17:13:44 -0500

    Change to google's go-github library to access GitHub's API

--- main.go
+++ main.go
@@ -2,23 +2,28 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 	"sort"
 
+	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
 )
 
+var client *github.Client
+
 func main() {
+	client = github.NewClient(nil)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
-	// r.HandleFunc("/user/{username:(?i)^[[a-z\\d]+-]*[a-z\\d]+$}", userHandler)
 	r.HandleFunc("/user/{username}", userHandler)
+
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
@@ -29,77 +34,46 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello on the container!")
 }
 
-// type people struct {
-// 	Number int `json:"number"`
-// }
-
-// Users struct which contains an array of users
-type Users struct {
-	Users []User `json:"users"`
-}
-
-// User struct which contains a name a type and a list of social links
-type User struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Age    int    `json:"Age"`
-	Social Social `json:"social"`
-}
-
-// Social struct which contains a list of links
-type Social struct {
-	Facebook string `json:"facebook"`
-	Twitter  string `json:"twitter"`
-}
-
-// Repo struct that contains a repository
-type Repo struct {
-	ID              int    `json:"id"`
-	Name            string `json:"name"`
-	Description     string `json:"description"`
-	Language        string `json:"language"`
-	HTMLURL         string `json:"html_url"`
-	Fork            bool   `json:"fork"`
-	StargazersCount int    `json:"stargazers_count"`
-	OpenIssues      int    `json:"open_issues_count"`
-	Forks           int    `json:"forks"`
-	CreatedAt       string `json:"created_at"`
-	PushedAt        string `json:"pushed_at"`
-	UpdatedAt       string `json:"updated_at"`
-}
-
 var reGithubUsername = regexp.MustCompile("(?i)^([a-z\\d]+-)*[a-z\\d]+$")
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	// fetch user repositories
 	vars := mux.Vars(r)
-	match := reGithubUsername.MatchString(vars["username"])
+	username := vars["username"]
+	match := reGithubUsername.MatchString(username)
 	if !match {
-		fmt.Fprintf(w, "Invalid Github username: %s\n", vars["username"])
-		fmt.Fprintf(os.Stderr, "ERROR: invalid github username: %s\n", vars["username"])
+		fmt.Fprintf(w, "Invalid Github username: %s\n", username)
+		fmt.Fprintf(os.Stderr, "ERROR: invalid github username: %s\n", username)
 		return
 	}
-	response := getGithubUserRepos(w, vars["username"])
-	data, _ := ioutil.ReadAll(response.Body)
 
-	// unmarshall bytes to json object
-	var repos []Repo
-	err := json.Unmarshal(data, &repos)
+	repos, resp, err := fetchRepositories(username, client)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Printf("Error: %v\n", err)
+		return
 	}
+
+	// copy headers from GitHub response
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 	// remove forked repositories
 	i := 0
 	for _, repo := range repos {
-		if !repo.Fork {
+		if !repo.GetFork() {
 			repos[i] = repo
 			i++
 		}
 	}
+
+	// sort repositories by stars
 	repos = repos[:i]
 	sort.Slice(repos, func(i, j int) bool {
-		return repos[i].StargazersCount > repos[j].StargazersCount
+		return repos[i].GetStargazersCount() > repos[j].GetStargazersCount()
 	})
 
 	// marshall json object to bytes
@@ -112,12 +86,8 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(json))
 }
 
-func getGithubUserRepos(w http.ResponseWriter, username string) (response *http.Response) {
-	response, err := http.Get("https://api.github.com/users/" + username + "/repos")
-	if err != nil {
-		fmt.Fprintf(w, "The HTTP request failed with error %s\n", err)
-		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-		os.Exit(1)
-	}
-	return response
+// fetchRepositories fetches all the public repositories of a user
+func fetchRepositories(username string, client *github.Client) ([]*github.Repository, *github.Response, error) {
+	repos, resp, err := client.Repositories.List(context.Background(), username, nil)
+	return repos, resp, err
 }
